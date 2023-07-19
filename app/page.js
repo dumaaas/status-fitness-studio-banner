fc8006452d079fd876fb0eb5c8008b6f4a83c2f9
Marko Dumnic <marko.dumnic@bild-studio.net>
2023-07-19 19:11:30 +0200

    First commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,116 @@
-import Image from 'next/image'
-
+"use client";
+import Image from "next/image";
+import { Sling as Hamburger } from "hamburger-react";
+import { useState } from "react";
+import img1 from "../public/assets/images/one.jpg";
+import img2 from "../public/assets/images/two.jpg";
+import img3 from "../public/assets/images/three.jpg";
 export default function Home() {
+  const [isOpen, setOpen] = useState(false);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex lg:flex-row flex-col h-screen w-screen relative overflow-hidden">
+      <header className="absolute top-0 left-0 w-full  px-[20px] py-[14px] flex items-center justify-between z-[999]">
+        <p className="text-white md:text-[32px] text-[26px]">Studio Status</p>
+        <Hamburger
+          duration={0.4}
+          easing="ease-in-out"
+          toggled={isOpen}
+          toggle={setOpen}
+        />
+      </header>
+      <div className="flex-[30%] bg-black h-full relative cursor-pointer group">
+        <div className="flex flex-col gap-[20px] items-center justify-center text-center h-full  group-hover:translate-y-[-20%] transform transition-all ease-in-out duration-[.4s]">
+          <p className="text-red-500 xl:text-[80px] lg:text-[64px] md:text-[52px] text-[42px] lg:group-hover:opacity-100 group-hover:opacity-0 transition-all ease-in-out duration-[.4s]">
+            PREMIUM
+          </p>
+          <p className="text-red-500 xl:text-[64px] lg:text-[54px] md:text-[44px] text-[34px] text-[24px] group-hover:opacity-0 group-hover:translate-y-[-20%] transform transition-all ease-in-out duration-[.4s]">
+            100 e
+          </p>
+        </div>
+        <div className="absolute top-0 left-0 w-full h-full ">
+          <Image
+            className="w-full h-full object-cover object-center opacity-0 group-hover:opacity-40 duration-[.5s] transition-all ease-in-out"
+            src={img1}
+            alt="trainer"
+          />
+        </div>
+        <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
+          <div className="sm:w-[80%] w-full px-[16px] sm:px-0 text-center flex flex-col gap-[40px] items-center justify-center">
+            <p className="xl:text-[22px] lg:text-[20px] md:text-[18px] text-[16px] group-hover:translate-y-0 translate-y-[20%] transition-all ease-in-out duration-[0.4s] opacity-0 group-hover:opacity-100 text-red-400">
+              Advanced training with expert trainers and a customized plan for
+              achieving top-notch results. Access to premium equipment and group
+              workouts. Comfortable space for relaxation and additional
+              amenities such as nutritional guidelines and individual
+              counseling.
+            </p>
+            <button className="absolute lg:relative bottom-[8px] right-[8px] rounded-[8px] md:px-[14px] md:py-[8px] px-[12px] py-[6px] hover:bg-white hover:border-red-500 bg-red-500 hover:text-red-500 border-2 md:text-[22px] sm:text-[20px] text-[18px] md:w-[150px] sm:w-[120px] w-[100px] hover:bg-transparent text-black border-black group-hover:translate-y-0 translate-y-[20%] transition-all ease-in-out duration-[0.4s] opacity-0 group-hover:opacity-100">
+              BUY NOW
+            </button>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="flex-[30%] bg-red-500 h-full relative cursor-pointer group">
+        <div className="flex flex-col gap-[20px] items-center justify-center text-center h-full  group-hover:translate-y-[-20%] transform transition-all ease-in-out duration-[.4s]">
+          <p className="text-black xl:text-[80px] lg:text-[64px] md:text-[52px] text-[42px] lg:group-hover:opacity-100 group-hover:opacity-0 transition-all ease-in-out duration-[.4s]">
+            VIP
+          </p>
+          <p className="text-black xl:text-[64px] lg:text-[54px] md:text-[44px] text-[34px] text-[24px] group-hover:opacity-0 group-hover:translate-y-[-20%] transform transition-all ease-in-out duration-[.4s]">
+            900 e
+          </p>
+        </div>
+        <div className="absolute top-0 left-0 w-full h-full ">
+          <Image
+            className="w-full h-full object-cover opacity-0 group-hover:opacity-40 duration-[.5s] transition-all ease-in-out"
+            src={img2}
+            alt="trainer"
+          />
+        </div>
+        <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
+          <div className="sm:w-[80%] w-full px-[16px] sm:px-0 text-center flex flex-col gap-[40px] items-center justify-center">
+            <p className="xl:text-[22px] lg:text-[20px] md:text-[18px] text-[16px] group-hover:translate-y-0 translate-y-[20%] transition-all ease-in-out duration-[0.4s] opacity-0 group-hover:opacity-100 text-gray-900">
+              Intensive training tailored to your goals with a personalized
+              program and individual trainer. Exclusive access to top-notch
+              equipment and group workouts. Luxurious relaxation space and
+              wellness services. Perfect for ambitious enthusiasts seeking the
+              highest level of support.
+            </p>
+            <button className="absolute lg:relative bottom-[8px] right-[8px] rounded-[8px] md:px-[14px] md:py-[8px] px-[12px] py-[6px] bg-black border-red-500 text-red-500 border-2 md:text-[22px] sm:text-[20px] text-[18px] md:w-[150px] sm:w-[120px] w-[100px] hover:bg-transparent hover:text-black border-black group-hover:translate-y-0 translate-y-[20%] transition-all ease-in-out duration-[0.4s] opacity-0 group-hover:opacity-100">
+              BUY NOW
+            </button>
+          </div>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+      <div className="flex-[30%] bg-black h-full relative cursor-pointer group">
+        <div className="flex flex-col gap-[20px] items-center justify-center text-center h-full  group-hover:translate-y-[-20%] transform transition-all ease-in-out duration-[.4s]">
+          <p className="text-red-500 xl:text-[80px] lg:text-[64px] md:text-[52px] text-[42px] lg:group-hover:opacity-100 group-hover:opacity-0 transition-all ease-in-out duration-[.4s]">
+            STANDARD
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          <p className="text-red-500 xl:text-[64px] lg:text-[54px] md:text-[44px] text-[34px] text-[24px] group-hover:opacity-0 group-hover:translate-y-[-20%] transform transition-all ease-in-out duration-[.4s]">
+            70 e
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        </div>
+        <div className="absolute top-0 left-0 w-full h-full ">
+          <Image
+            className="w-full h-full object-cover opacity-0 group-hover:opacity-40 duration-[.5s] transition-all ease-in-out"
+            src={img3}
+            alt="trainer"
+          />
+        </div>
+        <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
+          <div className="sm:w-[80%] w-full px-[16px] sm:px-0 text-center flex flex-col gap-[40px] items-center justify-center">
+            <p className="xl:text-[22px] lg:text-[20px] md:text-[18px] text-[16px] group-hover:translate-y-0 translate-y-[20%] transition-all ease-in-out duration-[0.4s] opacity-0 group-hover:opacity-100 text-red-400">
+              Quality training with a team of experienced trainers and a program
+              that suits your needs. Access to a well-equipped fitness center
+              and group workouts. An affordable option for those looking to
+              maintain fitness and improve health with expert guidance.
+            </p>
+            <button className="absolute lg:relative bottom-[8px] right-[8px] rounded-[8px] md:px-[14px] md:py-[8px] px-[12px] py-[6px] hover:bg-white hover:border-red-500 bg-red-500 hover:text-red-500 border-2 md:text-[22px] sm:text-[20px] text-[18px] md:w-[150px] sm:w-[120px] w-[100px] hover:bg-transparent text-black border-black group-hover:translate-y-0 translate-y-[20%] transition-all ease-in-out duration-[0.4s] opacity-0 group-hover:opacity-100">
+              BUY NOW
+            </button>
+          </div>
+        </div>
       </div>
-    </main>
-  )
+    </div>
+  );
 }
